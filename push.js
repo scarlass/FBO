7a1eb5bf020704d63cb4053942dbc7f7c7728c2b
scarlass <61391808+scarlass@users.noreply.github.com>
2020-11-03 05:01:50 +0700

    some changes

--- push.js
+++ push.js
@@ -33,9 +33,9 @@ const options = {
 
 wp.sendNotification(
   pushSubs({
-    end: "https://fcm.googleapis.com/fcm/send/fdtMJuXZbV4:APA91bE7e2HBS9kWhB8sr2j3ZyyGglANEQ7he3rTXaSOOOZhZMa26qKFF8sW7ZdZVfXu4bik-biBluiHlKmg7NFgI41lXUbULDGrVRk5VVDjkCCLo-v46O38aF0mIRcYoSjsNSMCSW_M",
-    p256: "BH3lLz4n9Ee+PKgFdAglk4f9VqYqxZd31R2HF7p2THt6cA680qPcHovS/kD+zjV3lOWeR6SeFAsQosIZ8FaM8EY=",
-    auth: "1FPmD+g6HiTCle1/dKWUdg=="
+    end: "",
+    p256: "",
+    auth: "",
   }),
   "WebPush: Hello :)",
   options
